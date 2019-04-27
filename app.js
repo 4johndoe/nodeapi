f76d3e599c14ab161f0f7b39017f4929a2b6296e
bogdan <bogdandemchenko@gmail.com>
2019-04-27 22:14:39 +0300

    basic post and get posts

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => console.log("DB Connected"));
+
+mongoose.connection.on('error', err => console.log(`DB connection error: ${err.message}`));
+
+// routes
+const postRoutes = require('./routes/post');
+
+// middleware
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use("/", postRoutes);
+
+// run
+const port = process.env.PORT || 8800;
+app.listen(port, () =>
+    console.log(`A Node Js API is listening port: ${port}`)
+);
