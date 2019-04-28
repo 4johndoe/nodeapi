81769acf97403fe02925649954b9523ae2281813
bogdan <bogdandemchenko@gmail.com>
2019-04-28 09:25:06 +0300

    select custom columns

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 
 exports.getPosts = (req, res) => {
-    const posts = Post.find()
+    const posts = Post.find().select("_id title body")
         .then((posts) => {
             res.status(200).json({posts: posts})
         })
